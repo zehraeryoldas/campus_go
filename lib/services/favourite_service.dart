1c70c73ce217867b59ac2efeff1bccf2e6ef5b9e
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-03-31 12:08:04 +0300

    favourite service created

---
+++ lib/services/favourite_service.dart
@@ -0,0 +1,34 @@
+import 'package:campusgo/models/favourite_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class favouriteService {
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  Future<String?> addFavourite(
+    String? post_id,
+    String? post_user_id,
+    int? status,
+    String? user_id,
+  ) async {
+    favouriteModel fmodel = favouriteModel(
+        post_id: post_id,
+        post_user_id: post_user_id,
+        status: status,
+        user_id: user_id);
+
+    String sonuc = "";
+
+    DocumentReference sonuc2 = FirebaseFirestore.instance
+        .collection("favorite")
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+
+    var userId = sonuc2.id;
+
+    DocumentReference sonuc3 = await FirebaseFirestore.instance
+        .collection("favorite")
+        .add(fmodel.toJson());
+
+    // var idx = sonuc1.id;
+    var ids = sonuc3.id;
+  }
+}
