ad66ae93e15c86b5dec14f845c6a0c28235cca43
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-04-08 12:03:48 +0300

    comment lines removed

--- lib/views/allAds.dart
+++ lib/views/allAds.dart
@@ -155,48 +155,5 @@ class _ilanlarimState extends State<allAds> {
     );
   }
 
-//   GridView Gridview(AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-//     return GridView.builder(
-//         itemCount: snapshot.data!.docs.length,
-//         gridDelegate:
-//             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-//         itemBuilder: (context, index) {
-//           DocumentSnapshot data = snapshot.data!.docs[index];
-//           return Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Stack(
-//               children: <Widget>[
-//                 Card(
-//                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0),side: BorderSide(color: Colors.white)),
-//                   child: Container(
-//                     //child:  Image.network("https://cdn.pixabay.com/photo/2022/10/10/14/13/lotus-7511897_1280.jpg",fit: BoxFit.cover,),
-//                     width: MediaQuery.of(context).size.height*0.20,
-//                     height: MediaQuery.of(context).size.height*0.80,
-//                     decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),color: mainColor.color,
-//                      image: DecorationImage(image: NetworkImage(data['images'].toString()),fit: BoxFit.cover)
-//                     ),
-//                   ),
-//                 ), //Container
-//                 Positioned(
-//                   top: MediaQuery.of(context).size.height*0.12,
-//                   child: Card(
-//                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0),side: BorderSide(color: Colors.white)),
-//                     child: Container(
-//                       width:  MediaQuery.of(context).size.height*0.20,
-//                       height: MediaQuery.of(context).size.height*0.100,
-//                       child: Column(children: [
-//                         ListTile(
-//                           subtitle: Text(data['name']),
-//                           title: Text(data['price'].toString() + " TL"),
-//                         ),
-//                       ]),
-//                     ),
-//                   ),
-//                 ) //Container)
-//               ], //<Widget>[]
-//             ),
-//           );
-//         },
-//       );
-//   }
+
 }
