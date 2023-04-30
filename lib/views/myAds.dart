1e3b4ddf19a95ae473cf36fdc0eb8fb8586aeb8a
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-04-30 15:34:25 +0300

    listview structure replaced with listview builder structure

--- lib/views/myAds.dart
+++ lib/views/myAds.dart
@@ -34,125 +34,122 @@ class _ilanlarimState extends State<ilanlarim> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Text("Loading");
           }
-          return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: ((context, index) {
+                DocumentSnapshot data = snapshot.data!.docs[index];
+                String idx = data['id']!.toString();
 
-            String idx = data['id']!.toString();
+                String id = data['userId']!;
+                String resim = data['images'].toString();
+                String name = data['name'].toString();
+                int price = data['price'];
+                String durum = data['status'];
+                String aciklama = data['description'];
+                String konum = data['location'];
+                String kategori = data['category_id'];
 
-            String id = data['userId']!;
-            String resim = data['images'].toString();
-            String name = data['name'].toString();
-            int price = data['price'];
-            String durum = data['status'];
-            String aciklama = data['description'];
-            String konum = data['location'];
-            String kategori = data['category_id'];
-
-            print("******");
-            print(data['name']);
-            print("******");
-
-            return GestureDetector(
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyAdsDetail(
-                              id: id,
-                              resim: resim,
-                              name: name,
-                              price: price,
-                              durum: durum,
-                              aciklama: aciklama,
-                              konum: konum,
-                              idx: idx,
-                              kategori: kategori,
-                            )));
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                // elevation: 10,
-                child: Row(children: [
-                  Container(
-                    //color: Colors.red,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.grey.shade300),
-                        image: DecorationImage(
-                            image: NetworkImage(data['images'].toString()))),
-                    width: MediaQuery.of(context).size.width * 0.40,
-                    height: MediaQuery.of(context).size.height * 0.12,
-                  ),
-                  Expanded(
-                    child: Container(
-                      width: 40,
-                      height: MediaQuery.of(context).size.height * 0.12,
-                      child: ListTile(
-                        title: Text(data['name'],
-                            style: TextStyle(
-                                color: Colors.black87,
-                                fontSize: 18.0,
-                                fontFamily: "RobotoCondensed")),
-                        subtitle: Text(data['price'].toString() + " \u20ba"),
-                        trailing: PopupMenuButton(
-                          child: Icon(Icons.more_vert),
-                          itemBuilder: ((context) => [
-                                PopupMenuItem(
-                                  child: Text("İlanı kaldır"),
-                                  value: 1,
-                                ),
-                                PopupMenuItem(
-                                  child: Text("İlanı Güncelle"),
-                                  value: 2,
-                                ),
-                              ]),
-                          onSelected: (menuItemValue) {
-                            if (menuItemValue == 1) {
-                              print("silindi");
-                              setState(() {
-                                FirebaseFirestore.instance
-                                    .collection("productss")
-                                    .where('name', isEqualTo: data['name'])
-                                    .get()
-                                    .then((snapshot) {
-                                  snapshot.docs.forEach((document) {
-                                    document.reference
-                                        .update({'productStatus': 0});
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MyAdsDetail(
+                                  id: id,
+                                  resim: resim,
+                                  name: name,
+                                  price: price,
+                                  durum: durum,
+                                  aciklama: aciklama,
+                                  konum: konum,
+                                  idx: idx,
+                                  kategori: kategori,
+                                )));
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    // elevation: 10,
+                    child: Row(children: [
+                      Container(
+                        //color: Colors.red,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.grey.shade300),
+                            image: DecorationImage(
+                                image:
+                                    NetworkImage(data['images'].toString()))),
+                        width: MediaQuery.of(context).size.width * 0.40,
+                        height: MediaQuery.of(context).size.height * 0.12,
+                      ),
+                      Expanded(
+                        child: Container(
+                          width: 40,
+                          height: MediaQuery.of(context).size.height * 0.12,
+                          child: ListTile(
+                            title: Text(data['name'],
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 18.0,
+                                    fontFamily: "RobotoCondensed")),
+                            subtitle:
+                                Text(data['price'].toString() + " \u20ba"),
+                            trailing: PopupMenuButton(
+                              child: Icon(Icons.more_vert),
+                              itemBuilder: ((context) => [
+                                    PopupMenuItem(
+                                      child: Text("İlanı kaldır"),
+                                      value: 1,
+                                    ),
+                                    PopupMenuItem(
+                                      child: Text("İlanı Güncelle"),
+                                      value: 2,
+                                    ),
+                                  ]),
+                              onSelected: (menuItemValue) {
+                                if (menuItemValue == 1) {
+                                  print("silindi");
+                                  setState(() {
+                                    FirebaseFirestore.instance
+                                        .collection("productss")
+                                        .where('name', isEqualTo: data['name'])
+                                        .get()
+                                        .then((snapshot) {
+                                      snapshot.docs.forEach((document) {
+                                        document.reference
+                                            .update({'productStatus': 0});
+                                      });
+                                    });
                                   });
-                                });
-                              });
-                            }
-                            if (menuItemValue == 2) {
-                              print("güncellendi");
-                              setState(() {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => MyAdsUpdate(
-                                              id: idx,
-                                              name: name,
-                                              aciklama: aciklama,
-                                              durum: durum,
-                                              idx: idx,
-                                              konum: konum,
-                                              kategori: kategori,
-                                              price: price,
-                                              resim: resim,
-                                            )));
-                              });
-                            }
-                          },
+                                }
+                                if (menuItemValue == 2) {
+                                  print("güncellendi");
+                                  setState(() {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => MyAdsUpdate(
+                                                  id: idx,
+                                                  name: name,
+                                                  aciklama: aciklama,
+                                                  durum: durum,
+                                                  idx: idx,
+                                                  konum: konum,
+                                                  kategori: kategori,
+                                                  price: price,
+                                                  resim: resim,
+                                                )));
+                                  });
+                                }
+                              },
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ]),
                   ),
-                ]),
-              ),
-            );
-          }).toList());
+                );
+              }));
         });
   }
 }
