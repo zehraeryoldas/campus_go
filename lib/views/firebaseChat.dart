f35486bc0e6aa8f60795a8dbc539ca050d747234
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-05-01 01:51:38 +0300

    timezone imported

--- lib/views/firebaseChat.dart
+++ lib/views/firebaseChat.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 
 class chat extends StatefulWidget {
   const chat({super.key});
@@ -11,7 +14,9 @@ class chat extends StatefulWidget {
 }
 
 class _chatState extends State<chat> {
+
   var flp = FlutterLocalNotificationsPlugin();
+
   Future<void> androidKurulum() async {
     var androidAyari =
         const AndroidInitializationSettings("@mipmap/ic_launcher");
@@ -37,6 +42,7 @@ class _chatState extends State<chat> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    //uygulama ilk açıldığında kurulum fonksiyonunu gerçekleştiriyoruz. Bununla işlerimizi halledebiliyoruz.
     androidKurulum();
   }
 
@@ -49,7 +55,30 @@ class _chatState extends State<chat> {
     var iosBildirimDetay=const DarwinNotificationDetails(); //detaylandırma gerek yok ios bunu kendi hallediyor.
     //bu iki yapıyı birleştirelim
     var bildirimDetay=NotificationDetails(android: androidBildirimDetay,iOS: iosBildirimDetay);
+    //mesajın gösterilmesi için başlık,içerik, detay bilgilerini verdik.
     await flp.show(0, "Zehra", "Son fiyat nedir", bildirimDetay,payload: "payload içerik");
+  }
+   Future<void> gecikmeliBildirimGoster() async {
+    var androidBildirimDetay = const AndroidNotificationDetails(
+        "kanal id", "kanal başlık",
+        channelDescription: "kanal açıklama",
+        priority: Priority.high,
+        importance: Importance.max);
+    var iosBildirimDetay=const DarwinNotificationDetails(); //detaylandırma gerek yok ios bunu kendi hallediyor.
+    //bu iki yapıyı birleştirelim
+    var bildirimDetay=NotificationDetails(android: androidBildirimDetay,iOS: iosBildirimDetay);
+    //mesajın gösterilmesi için başlık,içerik, detay bilgilerini verdik.
+   tz.initializeTimeZones();
+   var gecikme=tz.TZDateTime.now(tz.local).add(const Duration(seconds: 10)); //.add sayesinde gecikme ekliyoruz.mevcut zamanda gecikme yaptık.
+   await flp.zonedSchedule(0, "fatmanur",
+    "fiyat ne kadar",
+     gecikme, 
+     bildirimDetay, 
+     uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+     androidAllowWhileIdle: true //telefon ekranı kapalıyken bildirim geldiğinde ekranın uyanmasını sağlıyor.
+     ,payload: "Payload içerik gecikmeli" //bildirime tıklandığında bilgi eklenir.
+
+     );
   }
 
   @override
@@ -65,7 +94,9 @@ class _chatState extends State<chat> {
               bildirimGoster();
             }, child: Text("Bildirim olustur")),
             ElevatedButton(
-                onPressed: () {}, child: Text("Gecikmeli Bildirim olustur"))
+                onPressed: () {
+                  gecikmeliBildirimGoster();
+                }, child: Text("Gecikmeli Bildirim olustur"))
           ],
         ),
       ),
