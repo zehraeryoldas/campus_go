4963babfa8cc1a041cb693a48f5aee3ee9893fdc
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-05-01 22:47:08 +0300

    notification processes attempted

--- lib/views/urunDetayMesajlasma.dart
+++ lib/views/urunDetayMesajlasma.dart
@@ -7,6 +7,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'firebaseChat.dart';
 
 class urunDetayMesajlasma extends StatefulWidget {
   urunDetayMesajlasma({
@@ -38,36 +42,123 @@ class urunDetayMesajlasma extends StatefulWidget {
 class _urunDetayMesajlasmaState extends State<urunDetayMesajlasma> {
   TextEditingController messageController = TextEditingController();
 
- 
-
   void messageAdded(String text) {
-    
-      FirebaseFirestore.instance
-          .collection("productss")
-          .doc(widget.postId)
-          .collection("chats")
-          .add({
-        'message': text,
-        'timeStamp': DateTime.now(),
-        'senderId': FirebaseAuth.instance.currentUser!.uid,
-        'user_name': widget.user,
-        'receiverId': widget.postUserId
+    FirebaseFirestore.instance
+        .collection("productss")
+        .doc(widget.postId)
+        .collection("chats")
+        .add({
+      'message': text,
+      'timeStamp': DateTime.now(),
+      'senderId': FirebaseAuth.instance.currentUser!.uid,
+      'user_name': widget.user,
+      'receiverId': widget.postUserId
+    });
+    messageController.text = "";
+    bildirimGoster();
+  }
+
+  void messageRemoved(String text) {
+    FirebaseFirestore.instance
+        .collection("productss")
+        .doc(widget.postId)
+        .collection("chats")
+        .get()
+        .then((value) {
+      value.docs.forEach((document) {
+        document.reference.delete();
       });
-      messageController.text = "";
-    
+    });
+  }
+
+  var flp = FlutterLocalNotificationsPlugin();
+
+  Future<void> androidKurulum() async {
+    var androidAyari =
+        const AndroidInitializationSettings("@mipmap/ic_launcher");
+    var iosAyari = const DarwinInitializationSettings();
+    //ayarlar oluşturulduktan sonra birleştiriyorum
+    var kurulumAyari =
+        InitializationSettings(android: androidAyari, iOS: iosAyari);
+    await flp.initialize(kurulumAyari,
+        onDidReceiveNotificationResponse: bildirimSecilme);
+    //bu şekilde hem android hem ios için ayarlar verildi,
+    //birleştirildi kurulum gerçekleştrildi.
+  }
+
+  Future<void> bildirimSecilme(
+      NotificationResponse notificationResponse) async {
+    var payLoad = notificationResponse.payload;
+    if (payLoad != null) {
+      print("Bildirim seçildi,$payLoad");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //uygulama ilk açıldığında
+    //kurulum fonksiyonunu gerçekleştiriyoruz.
+    // Bununla işlerimizi halledebiliyoruz.
+    androidKurulum();
+  }
+
+  Future<void> bildirimGoster() async {
+    var androidBildirimDetay = const AndroidNotificationDetails(
+        "kanal id", "kanal başlık",
+        channelDescription: "kanal açıklama",
+        priority: Priority.high,
+        importance: Importance.max);
+    var iosBildirimDetay =
+        const DarwinNotificationDetails(); //detaylandırma gerek yok ios bunu kendi hallediyor.
+    //bu iki yapıyı birleştirelim
+    var bildirimDetay = NotificationDetails(
+        android: androidBildirimDetay, iOS: iosBildirimDetay);
+    //mesajın gösterilmesi için başlık,içerik, detay bilgilerini verdik.
+    await flp.show(0, widget.user, "fiyat nedir", bildirimDetay,
+        payload: "payload içerik");
+  }
+
+  Future<void> gecikmeliBildirimGoster() async {
+    var androidBildirimDetay = const AndroidNotificationDetails(
+        "kanal id", "kanal başlık",
+        channelDescription: "kanal açıklama",
+        priority: Priority.high,
+        importance: Importance.max);
+    var iosBildirimDetay =
+        const DarwinNotificationDetails(); //detaylandırma gerek yok ios bunu kendi hallediyor.
+    //bu iki yapıyı birleştirelim
+    var bildirimDetay = NotificationDetails(
+        android: androidBildirimDetay, iOS: iosBildirimDetay);
+    //mesajın gösterilmesi için başlık,içerik, detay bilgilerini verdik.
+    tz.initializeTimeZones();
+    var gecikme = tz.TZDateTime.now(tz.local).add(const Duration(
+        seconds:
+            10)); //.add sayesinde gecikme ekliyoruz.mevcut zamanda gecikme yaptık.
+    await flp.zonedSchedule(
+        0, "fatmanur", "fiyat ne kadar", gecikme, bildirimDetay,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        androidAllowWhileIdle:
+            true //telefon ekranı kapalıyken bildirim geldiğinde ekranın uyanmasını sağlıyor.
+        ,
+        payload:
+            "Payload içerik gecikmeli" //bildirime tıklandığında bilgi eklenir.
+
+        );
   }
 
   @override
   Widget build(BuildContext context) {
     //final String userId = "pceXDyA3HagfmzQ8vyXw8vokOaz1";
-     final Stream<QuerySnapshot> messageStream=FirebaseFirestore.instance
-              .collection("productss")
-              .doc(widget.postId)
-              .collection('chats')
-              .where("senderId",
-                  isEqualTo: FirebaseAuth.instance.currentUser!.uid)
-                
-              .snapshots();
+    final Stream<QuerySnapshot> messageStream = FirebaseFirestore.instance
+        .collection("productss")
+        .doc(widget.postId)
+        .collection('chats')
+        .where("senderId", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -193,13 +284,27 @@ class _urunDetayMesajlasmaState extends State<urunDetayMesajlasma> {
                   children: snapshot.data!.docs
                       .map((doc) => Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.yellow,
+                            child: Card(
+                              child: Container(
+                                decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(color: Colors.yellow)),
-                              child: ListTile(
-                                title: Text(doc['message']),
+                                ),
+                                child: ListTile(
+                                  title: Text(doc['message']),
+                                  trailing: PopupMenuButton(
+                                    itemBuilder: ((context) => [
+                                          PopupMenuItem(
+                                            child: Text("Mesajı sil"),
+                                            value: 1,
+                                          ),
+                                        ]),
+                                    onSelected: ((value) {
+                                      if (value == 1) {
+                                        messageRemoved(messageController.text);
+                                      }
+                                    }),
+                                  ),
+                                ),
                               ),
                             ),
                           ))
@@ -230,6 +335,7 @@ class _urunDetayMesajlasmaState extends State<urunDetayMesajlasma> {
                               IconButton(
                                   onPressed: () {
                                     messageAdded(messageController.text);
+                                    bildirimGoster();
                                   },
                                   icon: Icon(Icons.send))
                             ],
