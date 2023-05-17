82b829effc34ec3f037e940bd3aa8458dfc92ca7
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-05-17 16:45:31 +0300

    conversatin page changed

--- lib/arayuz.dart
+++ lib/arayuz.dart
@@ -30,7 +30,7 @@ class _AnaSayfaState extends State<Arayuz> {
       //ImageUploads(),
 
       const homePage(),
-      ConversationPage(),
+      Conversation(),
       const payPage(),
       adsPage(),
 
